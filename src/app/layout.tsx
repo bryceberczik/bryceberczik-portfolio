0cd84b0605abe27e0ae78c8fd269723b2a5c50d6
bryceberczik <berczikbryce@gmail.com>
2024-12-11 11:09:35 -0600

    completed starter/base header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./ui/header/Header";
+import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Bryce Berczik",
   description: "My portfolio for web development.",
 };
+
+const poppins = Poppins({
+  weight: ["400", "500"]
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-custom-image bg-cover backdrop-blur-lg antialiased">
+      <body className={`${poppins.className} bg-custom-image bg-cover backdrop-blur-lg antialiased`}>
         <Header />
         {children}
       </body>
--- src/app/ui/header/Header.tsx
+++ src/app/ui/header/Header.tsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faInbox } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
 
     return (
@@ -5,7 +9,7 @@ const Header = () => {
         <div>
             <header className="flex flex-row justify-around py-5">
                 <div>
-                    <h1 className="text-stone-300">BRYCE BERCZIK</h1>
+                    <h1 className="text-stone-300 ">BRYCE BERCZIK</h1>
                 </div>
                 <div className="flex flex-row">
                     <h1 className="mx-5 text-stone-300">ABOUT ME</h1>
@@ -14,8 +18,9 @@ const Header = () => {
                     <h1 className="mx-5 text-stone-300">CONTACT ME</h1>
                 </div>
                 <div className="flex flex-row">
-                    <h1 className="text-stone-300">D</h1>
-                    <h1 className="text-stone-300">E</h1>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    <FontAwesomeIcon icon={faGithub} />
+                    <FontAwesomeIcon icon={faInbox} />
                 </div>
             </header>
         </div>
